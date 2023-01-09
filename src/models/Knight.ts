59ccb1ad62aa346fc0e4630dfc5d3df7471cfaaf
7IBO <thibault.fayard.2001@gmail.com>
2023-01-09 22:56:31 +0100

    Add moves for Knight

--- src/models/Knight.ts
+++ src/models/Knight.ts
@@ -4,4 +4,24 @@ export class Knight extends Piece {
   constructor(x: number, y: number, color: "black" | "white") {
     super("knight", x, y, color);
   }
+
+  public getMovesPossible() {
+    this.movesPossible = [];
+
+    // TOP LEFT
+    this.addMovePossible(this.position.x - 2, this.position.y - 1);
+    this.addMovePossible(this.position.x - 1, this.position.y - 2);
+
+    // TOP RIGHT
+    this.addMovePossible(this.position.x + 2, this.position.y - 1);
+    this.addMovePossible(this.position.x + 1, this.position.y - 2);
+
+    // BOTTOM LEFT
+    this.addMovePossible(this.position.x - 2, this.position.y + 1);
+    this.addMovePossible(this.position.x - 1, this.position.y + 2);
+
+    // BOTTOM RIGHT
+    this.addMovePossible(this.position.x + 2, this.position.y + 1);
+    this.addMovePossible(this.position.x + 1, this.position.y + 2);
+  }
 }
